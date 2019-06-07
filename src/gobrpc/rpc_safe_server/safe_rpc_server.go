a3ad9a0534d5bff06e33a14c18f7755118351684
maruimm@foxmail.com <maruimm@foxmail.com>
2019-06-07 06:57:31 -0700

    提交最新的gobrpc

---
+++ src/gobrpc/rpc_safe_server/safe_rpc_server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	comm "gobrpc/comm"
+	"log"
+	"net"
+	"net/rpc"
+)
+
+type HelloService struct{}
+
+func (p *HelloService) Hello(request string, reply *string) error {
+	*reply = "hello:" + request
+	return nil
+}
+
+func main() {
+	comm.RegisterHelloService(new(HelloService))
+
+	listener, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		log.Fatal("ListenTCP error:", err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("Accept error:", err)
+		}
+
+		go rpc.ServeConn(conn)
+	}
+}
