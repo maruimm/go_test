1952ee6c4adc8eba3c2780cbbb66b1133e2904d9
maruimm <maruimm@foxmail.com>
2021-02-14 02:43:50 -0500

    增加heap insert/pop的操作

--- main.go
+++ main.go
@@ -6,8 +6,21 @@ import (
 )
 
 func main() {
-	h := heap.NewHeap(5)
+	h := heap.NewHeap([]heap.Elem{1,2,35,5,4,0})
 	fmt.Printf("befor adjust %+v\n",h )
 	h.Adjust()
 	fmt.Printf("after adjust %+v\n",h )
+	h.InsertNode(heap.Elem(77))
+	h.InsertNode(heap.Elem(77))
+	h.InsertNode(heap.Elem(6))
+	fmt.Printf("after insert %+v\n",h )
+	var p heap.Elem
+	for {
+		err := h.PopRootNode(&p)
+		if err != nil {
+			fmt.Printf("pop err:%+v\n", err)
+			break
+		}
+		fmt.Printf("pop elem:%+v, heap:%+v\n", p, h)
+	}
 }
--- src/myLib/heap/HeapSort.go
+++ src/myLib/heap/HeapSort.go
@@ -11,6 +11,7 @@ type Elem uint32
 var (
 	NoLeftChild = errors.New("no left child")
 	NoRightChild = errors.New("no right child")
+	NoNode = errors.New("no node in heap")
 )
 
 
@@ -20,52 +21,52 @@ type Heap struct {
 	height uint32
 }
 
-func (heep *Heap) leftChild(node uint32) (uint32, error){
+func (heap *Heap) leftChild(node uint32) (uint32, error){
 	lc := uint32(node << 1)
-	if lc > heep.size {
+	if lc > heap.size {
 		return 0, NoLeftChild
 	}
 	return lc , nil
 }
 
-func (heep *Heap) rightChild(node uint32) (uint32, error) {
-	lc ,err := heep.leftChild(node)
+func (heap *Heap) rightChild(node uint32) (uint32, error) {
+	lc ,err := heap.leftChild(node)
 	if err != nil {
 		return 0, err
 	}
 	rc := lc + 1
-	if rc > heep.size {
+	if rc > heap.size {
 		return 0, NoRightChild
 	}
 	return rc , nil
 }
 
 
-func (heep* Heap) Swap(i, j uint32) {
-	heep.els[i],heep.els[j] = heep.els[j],heep.els[i]
+func (heap * Heap) Swap(i, j uint32) {
+	heap.els[i], heap.els[j] = heap.els[j], heap.els[i]
 }
 
-func (heep* Heap) Compare(i, j uint32) bool{
-	if heep.els[i] > heep.els[j] {
+func (heap * Heap) Compare(i, j uint32) bool{
+	if heap.els[i] > heap.els[j] {
 		return true
 	}
 	return false
 }
 
-func (heep* Heap) SelectChild(adjustNode uint32) (uint32, error){
+func (heap * Heap) SelectChild(adjustNode uint32) (uint32, error){
 	var swapNode uint32
-	lc, err := heep.leftChild(uint32(adjustNode))
+	lc, err := heap.leftChild(uint32(adjustNode))
 	if err != nil { //不可能没有左子树
 		fmt.Printf("error :%s,adjustNode:%d\n", err, adjustNode)
 		return 0, err
 	}
-	rc, err := heep.rightChild(uint32(adjustNode))
+	rc, err := heap.rightChild(uint32(adjustNode))
 	if err != nil {
 		swapNode = lc //没有右子树，直接拿左子树比较
 		fmt.Printf("select child node:%d, adjustNode:%d\n", swapNode,adjustNode)
 		return swapNode, nil
 	}
-	if heep.Compare(lc, rc) {
+	if heap.Compare(lc, rc) {
 		swapNode = lc
 	} else {
 		swapNode = rc
@@ -74,8 +75,8 @@ func (heep* Heap) SelectChild(adjustNode uint32) (uint32, error){
 	return swapNode,nil
 }
 
-func (heep *Heap) Adjust() {
-	adjustLevel := heep.height - 1
+func (heap *Heap) Adjust() {
+	adjustLevel := heap.height - 1
 	for i := adjustLevel; i >= 1; i-- {
 		rightNode := (1 << i) - 1
 		leftNode := 1 << (i - 1)
@@ -84,15 +85,15 @@ func (heep *Heap) Adjust() {
 		for adjustNode := rightNode; adjustNode >= leftNode; adjustNode-- {
 			tmpadjustNode := uint32(adjustNode)
 			tmpi := i
-			for ;tmpi < heep.height; tmpi++ {
-				swapNode, err := heep.SelectChild(tmpadjustNode)
+			for ;tmpi < heap.height; tmpi++ {
+				swapNode, err := heap.SelectChild(tmpadjustNode)
 				if err != nil {
 					break
 				}
-				if heep.Compare(tmpadjustNode, swapNode) { //说明已经是大根堆了
+				if heap.Compare(tmpadjustNode, swapNode) { //说明已经是大根堆了
 					break
 				} else {
-					heep.Swap(tmpadjustNode, swapNode)
+					heap.Swap(tmpadjustNode, swapNode)
 					tmpadjustNode = swapNode
 				}
 			}
@@ -100,33 +101,72 @@ func (heep *Heap) Adjust() {
 	}
 }
 
+func (heap *Heap) InsertNode(val Elem) {
+	heap.els = append(heap.els, val)
+	heap.height = heap.calHeight()
+	heap.size = uint32(len(heap.els)) - 1
 
-func NewHeap(size uint32) *Heap{
-	//els := make([]Elem, 1, size + 1)
-
-	els := []Elem{0,1,4,3,2,5}
-
-	height := uint32(0)
-	tmpSize := size
-	for ;tmpSize != 0; tmpSize = tmpSize >> 1 {
-		height = height + 1
-	}
-
-	return &Heap {
-		size: size,
-		els: els,
-		height:height,
+	newNodeIdx := heap.size
+	for parentIndex := newNodeIdx >> 1; parentIndex != 0;  {
+		if heap.Compare(newNodeIdx, parentIndex) {
+			heap.Swap(parentIndex, newNodeIdx)
+			newNodeIdx = parentIndex
+			parentIndex = parentIndex >> 1
+		} else {
+			break
+		}
 	}
 }
 
+func (heap *Heap) PopRootNode(p *Elem) error {
+	if heap.size < 1 {
+		return NoNode
+	}
+	*p = heap.els[1]
+	heap.Swap(1, heap.size)
+	heap.els = heap.els[:heap.size]
+	heap.size = heap.size - 1
+	heap.height = heap.calHeight()
+	fmt.Printf("after swap:%+v\n", heap)
 
-/*
-func main() {
-	heep := NewHeap(5)
-	fmt.Printf("heep:%+v\n", heep)
+	parentNodeIdx := uint32(1)
+	for h := uint32(1); h < heap.height; h++ {
+		selectedChild,err := heap.SelectChild(parentNodeIdx)
+		if err != nil {
+			continue
+		}
+		if heap.Compare(selectedChild, parentNodeIdx) {
+			heap.Swap(selectedChild, parentNodeIdx)
+			parentNodeIdx = selectedChild
+		} else {
+			break
+		}
+	}
 
-	heep.Adjust()
+	return nil
+}
 
-	fmt.Printf("heep:%+v\n", heep)
 
-}*/
+func (heap* Heap) calHeight() uint32 {
+	size := heap.size
+	height := uint32(0)
+	for ; size != 0; size = size >> 1 {
+		height = height + 1
+	}
+	return height
+}
+
+func NewHeap(val []Elem) *Heap{
+	els := make([]Elem, 1)
+	els = append(els, val...)
+	size := uint32(len(val))
+
+	heap := Heap {
+		size: size,
+		els: els,
+	}
+	height := heap.calHeight()
+	heap.height = height
+	return &heap
+}
+
